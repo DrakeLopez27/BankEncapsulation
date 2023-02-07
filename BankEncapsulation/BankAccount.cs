2adf2282ab6f4267a246bf1ccdd6cc49220235cf
DrakeLopez27 <121637335+DrakeLopez27@users.noreply.github.com>
2023-02-07 09:48:59 -0600

    Finished ex.

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+     private readonly double balance;    
+        
+        public void Deposit(double amount)
+        {
+             amount += balance;
+        }
+        public double GetBalance() 
+        { 
+            return balance;
+        }
+
+    }
+    
+    
+    
+
+    
+    
+
+
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace BankEncapsulation
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var newAccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            var amountToDep = double.Parse(Console.ReadLine());
+
+            newAccount.Deposit(amountToDep);
+            double userBalance = newAccount.GetBalance();
+            Console.WriteLine($"Your balance is {amountToDep}");
+
+
+
         }
     }
 }
+
+
+//Create a new public class named BankAccount
+//In your BankAccount class, create the following:
+//A private field of type double named balance with a value of 0
+//Define a method named Deposit that will accept a double and store that value in the balance field
+//Define a method named GetBalance that will return the amount stored in the balance field
+//In the main method of your application, create a new instance of the BankAccount class. Allow the user of the application to Deposit money and retrieve their balance through the console.
+//Once finished save, commit, and push back to Github
